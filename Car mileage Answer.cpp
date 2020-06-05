49cc6074c1b230a6346c5403628d28cb3566e434
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-06-06 01:21:04 +0530

    Add files via upload

---
+++ Car mileage Answer.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+float m;
+
+int l,d;
+
+cin>>m>>l>>d;
+
+float x = m*l;
+
+if(x < d)
+
+cout<<"Cannot reach"<<endl;
+
+else
+
+cout<<"Can reach"<<endl;
+
+}
